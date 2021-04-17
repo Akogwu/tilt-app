57813c75267953d5c2b7febf0f35831fd9fffd01
Emmanuel <Emmanuelakogwuemmanuel57@gmail.com>
2021-04-17 15:33:19 +0100

    wip on whtoimage

---
+++ app/View/Components/SummaryResult.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\View\Components;
+
+use Illuminate\View\Component;
+
+class SummaryResult extends Component
+{
+    public $payment_status;
+    public $user;
+    public $testResult;
+    /**
+     * Create a new component instance.
+     *
+     * @return void
+     */
+    public function __construct($payment_status,$user,$testResult)
+    {
+        dd($payment_status);
+
+        $this->payment_status = $payment_status;
+        $this->user           = $user;
+        $this->testResult     = $testResult;
+    }
+
+    /**
+     * Get the view / contents that represent the component.
+     *
+     * @return \Illuminate\Contracts\View\View|string
+     */
+    public function render()
+    {
+        return view('components.summary-result');
+    }
+}
