f12e8e8ef5fd288babbde3ba857ca99146c295b3
Emmanuel <Emmanuelakogwuemmanuel57@gmail.com>
2021-04-20 06:59:40 +0100

    old

--- app/Http/Controllers/PDFController.php
+++ app/Http/Controllers/PDFController.php
@@ -4,13 +4,13 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Http;
-use PDF;
+//use PDF;
 
 class PDFController extends Controller
 {
-    function generateResult(){
-        
+    /*function generateResult(){
+
         return PDF::loadView('pages.results.complete')->stream('myResult.pdf');
 
-    }
+    }*/
 }
