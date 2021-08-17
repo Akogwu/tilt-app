2c324664837e5c4010e97bc2c17661f000f6c0e6
Meyanbe Olorunfemi <meyanbefemi97@gmail.com>
2021-08-17 05:53:26 +0100

    fix bug on creat school

--- resources/views/pages/school/create.blade.php
+++ resources/views/pages/school/create.blade.php
@@ -8,7 +8,7 @@
     <div>
         <div class="max-w-4xl mx-auto py-10 sm:px-6 lg:px-8">
             <div class="mt-5 md:mt-0 md:col-span-2">
-                <form method="post" action="{{ route('register.school') }}">
+                <form method="post" action="{{ route('school.register') }}">
                     @csrf
                     <div class="shadow overflow-hidden sm:rounded-md grid grid-cols-2 bg-white">
 
