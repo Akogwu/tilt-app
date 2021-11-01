bab29c8cd262647c1ff025dc863f6f5adc235cdf
Oluwatobi Sholanke <tobisholanke@gmail.com>
2021-11-01 12:05:03 +0100

    update

--- app/Http/Controllers/GraphOverviewCOntroller.php
+++ app/Http/Controllers/GraphOverviewCOntroller.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\GraphOverview;
 use Illuminate\Http\Request;
 
-class GraphOverviewCOntroller extends Controller
+class GraphOverviewController extends Controller
 {
 
     public function getOverview(){
