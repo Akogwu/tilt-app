1270aa51f98084fe917e2c932503cf3f3c32ebbc
Oluwatobi Sholanke <tobisholanke@gmail.com>
2021-11-17 10:49:40 +0100

    updated

--- resources/views/pages/result.blade.php
+++ resources/views/pages/result.blade.php
@@ -14,7 +14,26 @@ echo "</script>";
 
 
 ?>
+<style>
 
+.page-loader {
+	position: absolute;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    z-index: 900000;
+    background: #fff;
+}
+
+.page-loader .holder {
+	width: max-content;
+	margin: auto;
+	margin-top: 350px;
+}
+
+
+</style>
 
 <div id="page-loader" class="page-loader">
         <div class="holder">
