c42fd21077f8f9059022c199c5614df3993eadcd
Meyanbe Olorunfemi <meyanbefemi97@gmail.com>
2022-01-17 19:26:46 +0100

    remove headers

--- app/Models/PrivateLearner.php
+++ app/Models/PrivateLearner.php
@@ -25,4 +25,12 @@ class PrivateLearner extends Model
     public function user(){
         return $this->belongsTo(User::class, 'user_id');
     }
+
+    public function country(){
+        return $this->belongsTo(Country::class, 'country_id');
+    }
+
+    public function state(){
+        return $this->belongsTo(StateProvince::class, 'state_id');
+    }
 }
